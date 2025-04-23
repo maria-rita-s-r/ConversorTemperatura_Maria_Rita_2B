1b80f9b9a2890b884ea7d9b10026a343c1c3bc3e
2024105070041 <2024105070041@aqs-lab-c-08.mshome.net>
2025-04-23 15:39:34 -0300

    Segundo Commit

--- src/main/java/ConversorTemperatura.java
+++ src/main/java/ConversorTemperatura.java
@@ -1,11 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
-/**
- *
- * @author 2024105070041
- */
 public class ConversorTemperatura {
 
     public static void main(String[] args) {
